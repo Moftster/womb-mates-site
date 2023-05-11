64157d606d07974a23d84dff8f720de954e4eb94
David Moftakhar <davidmoftakhar@gmail.com>
2023-05-11 21:26:39 +0100

    Responsive design.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
     <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:p-10">
             <div class="overflow-hidden">
-                <div class="grid grid-cols-7">
-                    <div class="p-12 col-span-4 space-y-12">
+                <div class="grid lg:grid-cols-7 content-center">
+                    <div class="p-12 lg:col-span-4 space-y-12">
                         <div class="space-y-7">
                             <h5 class="text-5xl font-bold">Track Twin Feeding</h5>
 
@@ -17,16 +17,16 @@
                         <div class="space-y-7">
                             <p class="font-bold text-lg">Coming soon on the App Store and Google Play.</p>
 
-                            <div class="grid grid-cols-6 gap-8">
+                            <div class="flex space-x-6">
 
-                                <img class="cursor-pointer" src="{{ asset('logos/app-store-badge.svg') }}" alt="App Store Badge">
+                                <img class="cursor-pointer w-28" src="{{ asset('logos/app-store-badge.svg') }}" alt="App Store Badge">
 
-                                <img class="cursor-pointer" src="{{ asset('logos/google-play-badge.svg') }}" alt="App Store Badge">
+                                <img class="cursor-pointer w-28" src="{{ asset('logos/google-play-badge.svg') }}" alt="App Store Badge">
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-span-3">
+                    <div class="lg:col-span-3">
                         <img class="cursor-pointer mx-auto py-12" src="{{ asset('logos/phone.png') }}" alt="App Store Badge">
                     </div>
                 </div>
