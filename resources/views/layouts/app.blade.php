fde5ac0a49ecda95b621488bd9cfeca1e7036bf5
David Moftakhar <davidmoftakhar@gmail.com>
2023-05-11 21:27:09 +0100

    Footer WIP.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen>
+        <div class="min-h-screen flex flex-col">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -28,9 +28,11 @@
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="flex-grow">
                 {{ $slot }}
             </main>
+
+            @include('layouts.footer')
         </div>
     </body>
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="bg-gray-100 p-8 flex flex-col items-center">
+    <div class="container mx-auto px-4">
+        <div class="space-y-2 text-center">
+            <div>
+                <ul class="flex justify-center space-x-4">
+                    <li><a href="#" class="text-gray-600 hover:text-gray-800">Home</a></li>
+                    <li><a href="#" class="text-gray-600 hover:text-gray-800">About</a></li>
+                    <li><a href="#" class="text-gray-600 hover:text-gray-800">Contact</a></li>
+                </ul>
+            </div>
+            <div>
+                <span class="text-gray-600">&copy; 2023 Womb Mates</span>
+            </div>
+        </div>
+    </div>
+</footer>
