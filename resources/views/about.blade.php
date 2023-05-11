4060fe3525dfce8748cc743eea6c5e8358fcc05c
David Moftakhar <davidmoftakhar@gmail.com>
2023-05-11 20:03:27 +0100

    Homepage overview.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,11 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h3>Welcome to the About Page</h3>
+
+
+                    <div>
+                        <p>If you're a parent of twins, you know how challenging it can be to keep track of their feeding schedules. That's why our app is designed exclusively for parents of twins, to make it easier to track and manage their feeding routines. With features such as simultaneous feeding tracking and customizable reminders, our app is the perfect tool to help you stay on top of your twins' feeding needs. Say goodbye to the stress and confusion of feeding twins, and try our app today!</p>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,34 @@
 <x-app-layout>
     <div class="">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden">
-                <div class="grid grid-cols-6">
-                    <div class="p-12 col-span-4 bg-blue-50 space-y-6">
-                        <h5 class="text-5xl">Track Twin Feeding</h5>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:p-10">
+            <div class="overflow-hidden">
+                <div class="grid grid-cols-7">
+                    <div class="p-12 col-span-4 space-y-12">
+                        <div class="space-y-7">
+                            <h5 class="text-5xl font-bold">Track Twin Feeding</h5>
 
-                        <h5 class="text-5xl">Monitor your <span class="font-bold">babies progress</span></h5>
+                            <h5 class="text-5xl font-bold">Monitor Your</h5>
 
-                        <p>Womb Mates is a twin feeding tracker & planner for iOS and Android. The only app focussed on monitoring and tracking your twins feeding.</p>
-
-                        <p class="font-bold">Coming soon on the App Store and Google Play.</p>
-
-                        <div class="grid grid-cols-6 gap-8">
-                            <img class="cursor-pointer" src="{{ asset('logos/app-store-badge.svg') }}" alt="App Store Badge">
-
-                            <img class="cursor-pointer" src="{{ asset('logos/google-play-badge.svg') }}" alt="App Store Badge">
+                            <h5 class="text-5xl font-bold text-blue-600">Babies Progress</h5>
                         </div>
 
+                        <p class="text-xl">Womb Mates is a twin feeding tracker & planner for iOS and Android. The only app focussed on monitoring and tracking your twins feeding.</p>
 
+                        <div class="space-y-7">
+                            <p class="font-bold text-lg">Coming soon on the App Store and Google Play.</p>
+
+                            <div class="grid grid-cols-6 gap-8">
+
+                                <img class="cursor-pointer" src="{{ asset('logos/app-store-badge.svg') }}" alt="App Store Badge">
+
+                                <img class="cursor-pointer" src="{{ asset('logos/google-play-badge.svg') }}" alt="App Store Badge">
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="col-span-2 bg-red-500">
+                    <div class="col-span-3">
+                        <img class="cursor-pointer mx-auto py-12" src="{{ asset('logos/phone.png') }}" alt="App Store Badge">
                     </div>
-                </div>
-
-                <div>
-                    <p>If you're a parent of twins, you know how challenging it can be to keep track of their feeding schedules. That's why our app is designed exclusively for parents of twins, to make it easier to track and manage their feeding routines. With features such as simultaneous feeding tracking and customizable reminders, our app is the perfect tool to help you stay on top of your twins' feeding needs. Say goodbye to the stress and confusion of feeding twins, and try our app today!</p>
                 </div>
             </div>
         </div>
