c616da1bec0d6e08315d32a1ad5dac6e12e31302
David Moftakhar <davidmoftakhar@gmail.com>
2023-05-12 13:22:25 +0100

    Basic footer.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Steps to get the project running on your local machine:
 
 - cd womb-mates-site
-- ./vendor/bin/sail up
+- ./vendor/bin/sail up -d
 - ./vendor/bin/sail npm run dev
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,14 @@
 <footer class="bg-gray-100 p-8 flex flex-col items-center">
     <div class="container mx-auto px-4">
-        <div class="space-y-2 text-center">
+        <div class="space-y-2 text-center text-sm">
             <div>
                 <ul class="flex justify-center space-x-4">
-                    <li><a href="#" class="text-gray-600 hover:text-gray-800">Home</a></li>
-                    <li><a href="#" class="text-gray-600 hover:text-gray-800">About</a></li>
-                    <li><a href="#" class="text-gray-600 hover:text-gray-800">Contact</a></li>
+                    <li><a href="{{ route('home') }}" class="text-gray-600 hover:text-gray-800">Home</a></li>
+                    <li><a href="{{ route('about') }}" class="text-gray-600 hover:text-gray-800">About</a></li>
                 </ul>
             </div>
             <div>
-                <span class="text-gray-600">&copy; 2023 Womb Mates</span>
+                <span class="text-gray-600">&copy; Womb Mates <script>document.write(/\d{4}/.exec(Date())[0])</script></span>
             </div>
         </div>
     </div>
